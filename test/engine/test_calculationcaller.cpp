a2b65587e59b233619bfaf762d34319563167769
jandricd <jandricd@mcmaster.ca>
2023-03-07 15:26:46 -0500

    Add more tests to calculationcaller

--- test/engine/test_calculationcaller.cpp
+++ test/engine/test_calculationcaller.cpp
@@ -2,28 +2,234 @@
 
 #include "../../src/engine/calculationcaller.hpp"
 #include "../../src/report/report_mocks.hpp"
+#include "MatlabDataArray.hpp"
 
 class TestCalculationCaller : public QObject {
   Q_OBJECT
 
+ private:
+  std::shared_ptr<matlab::cpplib::MATLABLibrary> m;
  private slots:
-  void testMatlabInit() {
-    auto m = mtobridge::getMatlab();
-    Q_ASSERT(m.get());
+  void initTestCase() {
+    m = mtobridge::getMatlab();
+    QVERIFY(m.get());
   }
 
-  void testMatLabDestroy() {
-    auto m = mtobridge::getMatlab();
-    mtobridge::destroyMatlab();
-    Q_ASSERT(m != nullptr);
+  void testValidCommand() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    mtobridge::MockCalculationOutputT out;
+    QVERIFY_THROWS_NO_EXCEPTION(out = mtobridge::runCalculation(in));
+    QCOMPARE(out.firstAxlePosition.size(),
+             std::ceil(((3.6576 + 1.2192 + 9.4488 + 1.2192) * 3 + 5 * 2 + 40) /
+                       0.1));
+    QCOMPARE(out.firstAxlePosition.size(), out.forceConcernedSection.size());
+
+    // critical section should not be defined
+    QCOMPARE(out.criticalSection, -1);
+    QVERIFY(out.forceCriticalSection.empty());
   }
 
-  void testMatlabRunCommand() {
-    mtobridge::MockCalculationInputT in = {.truckConfig = {.axleLoad = {},
-                                                           .axleSpacing = {},
-                                                           .numberOfTrucks = 1,
-                                                           .headway = 5}};
+  void testInvalidCommandAxleLoad() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
   }
+
+  void testInvalidCommandAxleSpacing1() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandAxleSpacing2() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandNumberOfTrucks() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 0,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandHeadway() {
+    QSKIP(
+        "Headway of -1 causes failure with debug assertion, skip to run other "
+        "tests.");
+    return;
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = -1},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandNumberOfSpans1() {
+    QSKIP(
+        "0 spans causes failure with debug assertion, skip to run other "
+        "tests.");
+    return;
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 0,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandNumberOfSpans2() {
+    QSKIP("10 spans causes segfault, skip to run other tests.");
+    return;
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 10,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandSpanLength1() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandSpanLength2() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {-1, -1},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandConcernedSection() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = -1,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandDiscretizationLength1() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 70},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void testInvalidCommandDiscretizationLength2() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = -1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::runCalculation(in));
+  }
+
+  void cleanupTestCase() { mtobridge::destroyMatlab(); }
 };
 
 QTEST_MAIN(TestCalculationCaller)
