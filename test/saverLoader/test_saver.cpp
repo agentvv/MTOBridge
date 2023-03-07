4e1429411e21db237a38d6d96b8f1c4fe20f80aa
Pedram Yazdinia <32027117+yazdinip@users.noreply.github.com>
2023-03-07 18:59:32 -0500

    added test units for saver

---
+++ test/saverLoader/test_saver.cpp
@@ -0,0 +1,119 @@
+#include <QTest>
+
+#include "../../src/saver/saver.hpp"
+#include "../../src/report/report_mocks.hpp"
+#include "MatlabDataArray.hpp"
+
+class TestCalculationCaller : public QObject {
+  Q_OBJECT
+
+ private:
+  
+ private slots:
+
+
+  void testValidSavePlatoonConfiguration() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    //mtobridge::MockCalculationOutputT out;
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::saver::savePlatoonConfiguration(in.truckConfig));
+  }
+  
+
+ void testInvalidSavePlatoonConfiguration() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    // mtobridge::MockCalculationOutputT out;
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::saver::savePlatoonConfiguration(in.truckConfig));
+  }
+
+   void testValidSaveBridgeConfiguration() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    // mtobridge::MockCalculationOutputT out;
+    //QVERIFY_THROWS_NO_EXCEPTION(mtobridge::saver::saveBridgeConfiguration(in.bridgeConfig));
+  }
+
+  void testInvalidSaveBridgeConfiguration() {
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    // mtobridge::MockCalculationOutputT out;
+    //QVERIFY_THROWS_NO_EXCEPTION(mtobridge::saver::saveBridgeConfiguration(in.bridgeConfig));
+  }
+
+  void testValidSaveSolverConfiguration() {
+    QSKIP("Hello");
+    return;
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    // mtobridge::MockCalculationOutputT out;
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::saver::saveSolverConfiguration(in.solverConfig));
+  }
+ 
+    void testInvalidSaveSolverConfiguration() {
+    QSKIP("Hello");
+    return;
+    mtobridge::MockCalculationInputT in = {
+        .truckConfig = {.axleLoad = {53.4, 75.6, 75.6, 75.6, 75.6},
+                        .axleSpacing = {3.6576, 1.2192, 9.4488, 1.2192},
+                        .numberOfTrucks = 3,
+                        .headway = 5},
+        .bridgeConfig = {.numberSpans = 2,
+                         .spanLength = {20, 20},
+                         .concernedSection = 10,
+                         .discretizationLength = 0.1},
+        .solverConfig = {.forceType = mtobridge::MockSolverT::POSITIVE_MOMENT,
+                         .solverType = mtobridge::MockSolverT::CONCERNED}};
+    // mtobridge::MockCalculationOutputT out;
+    QVERIFY_THROWS_NO_EXCEPTION(mtobridge::saver::saveSolverConfiguration(in.solverConfig));
+  }
+
+};
+
+QTEST_MAIN(TestCalculationCaller)
+#include "test_saver.moc"
